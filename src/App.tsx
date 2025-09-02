320b3293f44b8f3266442dec90c701645d58cadc
AlwaysAsLearner <prathamesharjunsawant@gmail.com>
2025-09-02 13:58:50 +0530

    Intial frontend commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+     <div className='min-h-screen bg-blue-500 flex items-center justify-center'>
+      <h1 className="text-white text-center text-7xl">
+        Hello Chat App
+      </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
